762028e59b54baff88b43d937d951bd8efed4aed
baliuzeger <baliuzeger@gmail.com>
2019-06-10 13:35:31 +0800

    initial design of traits Acitce & Passive to replace Runnable.

--- src/core/src/agents.rs
+++ src/core/src/agents.rs
@@ -2,6 +2,7 @@ pub mod neurons;
 pub mod synapses;
 // pub mod devices;
 
+pub trait Agent {}
 pub trait Device {}
 pub use neurons::Neuron;
 pub use synapses::Synapse;
--- src/core/src/operation.rs
+++ src/core/src/operation.rs
@@ -4,6 +4,7 @@ use crossbeam_channel::Receiver as CCReceiver;
 use crossbeam_channel::Sender as CCSender;
 use std::thread;
 use std::thread::JoinHandle;
+use crate::{AcMx};
 
 pub mod op_population;
 pub mod op_agent;
@@ -79,13 +80,25 @@ pub trait ActiveAgent: Configurable {}
 /// for PassivePopulation & connectivity / OutComponents
 pub trait PassiveAgent: Runnable<Confirm = Broadcast, Report = ()> + Configurable {}
 
-// pub trait PassiveAgent: Configurable {
-//     fn super_confirm_sender(&mut self) -> CCSender<Broadcast>;
-//     fn super_confirm_receiver(&mut self) -> CCReceiver<Broadcast>;
-//     fn make_fore_ch_pair(&mut self) -> (CCSender<Broadcast>, CCReceiver<()>);
-//     fn fore_ch_pairs(&mut self) -> Vec<(CCReceiver<Broadcast>, CCSender<()>)>;
+pub trait Active {
+    // type Confirm: Send;
+    type Report: Send;
+    fn run(&mut self);
+    fn confirm_sender(&self) -> CCSender<Broadcast>;
+    fn confirm_receiver(&self) -> CCReceiver<Broadcast>;
+    fn report_receiver(&self) -> CCReceiver<<Self as Active>::Report>;
+    fn report_sender(&self) -> CCSender<<Self as Active>::Report>;
+}
+
+pub trait Passive {
+    fn run(&mut self);
+    fn confirm_sender(&self) -> CCSender<Broadcast>;
+    fn confirm_receiver(&self) -> CCReceiver<Broadcast>;
     
-// }
+    // fn make_fore_ch_pair(&mut self) -> (CCSender<Broadcast>, CCReceiver<()>);
+    // fn fore_ch_pairs(&mut self) -> Vec<(CCReceiver<Broadcast>, CCSender<()>)>;
+    
+}
 
 pub struct RunningSet<C: Send, R: Send> {
     pub instance: JoinHandle<()>,
@@ -108,19 +121,67 @@ impl<C: Send, R: Send> RunningSet<C, R> {
     }
 }
 
-// pub struct PassiveRunningSet<C: Send> {
-//     pub instance: JoinHandle<()>,
-//     pub confirm: CCSender<C>,
-// }
-
-// impl<C: Send> PassiveRunningSet<C> {
-//     pub fn new<T>(agent: Arc<Mutex<T>>) -> RunningSet<<T as Runnable>::Confirm>
+// impl<C: Send, R: Send> RunningSet<C, R> {
+//     pub fn new<T>(agent: Arc<Mutex<T>>) -> RunningSet<<T as Runnable>::Confirm, <T as Runnable>::Report>
 //     where T: 'static + Runnable + Send + ?Sized
 //     {
-//         let confirm = agent.lock().unwrap().super_confirm_sender();
+//         // for strict ordering of agent-connection_prop, bounded(1) is chosen.
+//         let unwrapped_agent = agent.lock().unwrap();
+//         print!("Agent locked within RunningSet::new().");
 //         RunningSet {
-//             instance: thread::spawn(move || {agent.lock().unwrap().run_passive()}),
-//             confirm,
+//             confirm: unwrapped_agent.confirm_sender(),
+//             report: unwrapped_agent.report_receiver(),
+//             // instance: thread::spawn(move || unwrapped_agent.run()),
+//             instance: thread::spawn(move || {agent.lock().unwrap().run()}),
 //         }
 //     }
 // }
+
+pub struct PassiveRunningSet {
+    pub instance: JoinHandle<()>,
+    pub confirm: CCSender<Broadcast>,
+}
+
+impl PassiveRunningSet {
+    pub fn new<T>(agent: AcMx<T>) -> PassiveRunningSet
+    where T: 'static + Passive + Send + ?Sized
+    {
+        let confirm = agent.lock().unwrap().confirm_sender();
+        PassiveRunningSet {
+            instance: thread::spawn(move || {agent.lock().unwrap().run()}),
+            confirm,
+        }
+    }
+}
+
+pub struct ActiveRunningSet<R> {
+    pub instance: JoinHandle<()>,
+    pub confirm: CCSender<Broadcast>,
+    pub report: CCReceiver<R>,
+}
+
+impl<R> ActiveRunningSet<R> {
+    pub fn new<T>(agent: AcMx<T>) -> ActiveRunningSet<<T as Active>::Report>
+    where T: 'static + Active + Send + ?Sized
+    {
+        let mut confirm;
+        let mut report;
+        {
+            let unwraped_agent = agent.lock().unwrap();
+            confirm = unwraped_agent.confirm_sender();
+            report = unwraped_agent.report_receiver();            
+        }
+        ActiveRunningSet {
+            confirm,
+            report,
+            instance: thread::spawn(move || {agent.lock().unwrap().run()}),
+        }
+        
+        // let unwrapped_agent = agent.lock().unwrap();
+        // ActiveRunningSet {
+        //     confirm: unwrapped_agent.confirm_sender(),
+        //     report: unwrapped_agent.report_receiver(),
+        //     instance: thread::spawn(move || {agent.lock().unwrap().run()}),
+        // }
+    }
+}

28c697c9566dacaa5f1b66be8b18242a95eeac8d
baliuzeger <baliuzeger@gmail.com>
2019-06-11 17:38:29 +0800

    change ActiveRunningSet::new() as Option<>.

--- src/core/src/operation.rs
+++ src/core/src/operation.rs
@@ -120,22 +120,6 @@ impl<C: Send, R: Send> RunningSet<C, R> {
     }
 }
 
-// impl<C: Send, R: Send> RunningSet<C, R> {
-//     pub fn new<T>(agent: Arc<Mutex<T>>) -> RunningSet<<T as Runnable>::Confirm, <T as Runnable>::Report>
-//     where T: 'static + Runnable + Send + ?Sized
-//     {
-//         // for strict ordering of agent-connection_prop, bounded(1) is chosen.
-//         let unwrapped_agent = agent.lock().unwrap();
-//         print!("Agent locked within RunningSet::new().");
-//         RunningSet {
-//             confirm: unwrapped_agent.confirm_sender(),
-//             report: unwrapped_agent.report_receiver(),
-//             // instance: thread::spawn(move || unwrapped_agent.run()),
-//             instance: thread::spawn(move || {agent.lock().unwrap().run()}),
-//         }
-//     }
-// }
-
 pub struct PassiveRunningSet {
     pub instance: JoinHandle<()>,
     pub confirm: CCSender<Broadcast>,
@@ -150,7 +134,6 @@ impl PassiveRunningSet {
             RunMode::Idle => None,
             RunMode::ForwardStepping => {
                 let confirm = agent.lock().unwrap().confirm_sender();
-                println!("PassiveRunningSet.new().");
                 Some(PassiveRunningSet {
                     instance: thread::spawn(move || {agent.lock().unwrap().run()}),
                     confirm,
@@ -168,29 +151,23 @@ pub struct ActiveRunningSet<R> {
 }
 
 impl<R> ActiveRunningSet<R> {
-    pub fn new<T>(agent: AcMx<T>) -> ActiveRunningSet<<T as Active>::Report>
+    pub fn new<T>(agent: AcMx<T>) -> Option<ActiveRunningSet<<T as Active>::Report>>
     where T: 'static + Active + Send + ?Sized
     {
-        let mut confirm;
-        let mut report;
-        {
-            let unwraped_agent = agent.lock().unwrap();
-            confirm = unwraped_agent.confirm_sender();
-            report = unwraped_agent.report_receiver();            
+        let m = agent.lock().unwrap().mode();
+        match m {
+            RunMode::Idle => None,
+            RunMode::ForwardStepping => {
+                let confirm = agent.lock().unwrap().confirm_sender();
+                let report = agent.lock().unwrap().report_receiver();            
+                Some(ActiveRunningSet {
+                    instance: thread::spawn(move || {agent.lock().unwrap().run()}),
+                    confirm,
+                    report,
+                })
+            },
+            RunMode::ForwardRealTime => panic!("ForwardRealTime not yet implemented!"),
         }
-        println!("ActiveRunningSet::new()");
-        ActiveRunningSet {
-            confirm,
-            report,
-            instance: thread::spawn(move || {agent.lock().unwrap().run()}),
-        }
-        
-        // let unwrapped_agent = agent.lock().unwrap();
-        // ActiveRunningSet {
-        //     confirm: unwrapped_agent.confirm_sender(),
-        //     report: unwrapped_agent.report_receiver(),
-        //     instance: thread::spawn(move || {agent.lock().unwrap().run()}),
-        // }
     }
 }
 
